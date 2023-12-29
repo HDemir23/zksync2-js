1fedef564043b3b6bd0b48a3aa4e22c5947e29df
Hakan <a.hakandemir23@gmail.com>
2023-12-29 06:26:22 +0300

    wallet.ts

--- src/wallet.ts
+++ src/wallet.ts
@@ -12,6 +12,9 @@ export class Wallet extends AdapterL2(AdapterL1(ethers.Wallet)) {
     // @ts-ignore
     public eip712: EIP712Signer;
 
+    // Methods managing L1, L2, and EIP712 provider
+
+    // Returns L1 provider
     override _providerL1() {
         if (this.providerL1 == null) {
             throw new Error("L1 provider missing: use `connectToL1` to specify");
@@ -19,35 +22,43 @@ export class Wallet extends AdapterL2(AdapterL1(ethers.Wallet)) {
         return this.providerL1;
     }
 
+    // Returns L2 provider
     override _providerL2() {
         return this.provider;
     }
 
+    // Returns signer for L1
     override _signerL1() {
         return this.ethWallet();
     }
 
+    // Returns signer for L2
     override _signerL2() {
         return this;
     }
 
+    // Creates an Ethereum wallet
     ethWallet(): ethers.Wallet {
         return new ethers.Wallet(this.signingKey, this._providerL1());
     }
 
+    // Connects to L2 provider
     override connect(provider: Provider): Wallet {
         return new Wallet(this.signingKey, provider, this.providerL1);
     }
 
+    // Connects to L1 provider
     connectToL1(provider: ethers.Provider): Wallet {
         return new Wallet(this.signingKey, this.provider, provider);
     }
 
+    // Creates a wallet from mnemonic
     static fromMnemonic(mnemonic: string, provider?: ethers.Provider): Wallet {
         const wallet = super.fromPhrase(mnemonic, provider);
         return new Wallet(wallet.signingKey, undefined, wallet.provider as ethers.Provider);
     }
 
+    // Creates a wallet from encrypted JSON (asynchronous)
     static override async fromEncryptedJson(
         json: string,
         password: string | Uint8Array,
@@ -57,11 +68,13 @@ export class Wallet extends AdapterL2(AdapterL1(ethers.Wallet)) {
         return new Wallet(wallet.signingKey);
     }
 
+    // Creates a wallet from encrypted JSON (synchronous)
     static override fromEncryptedJsonSync(json: string, password: string | Uint8Array): Wallet {
         const wallet = super.fromEncryptedJsonSync(json, password);
         return new Wallet(wallet.signingKey);
     }
 
+    // Constructor to create a wallet
     constructor(
         privateKey: string | ethers.SigningKey,
         providerL2?: Provider,
@@ -80,47 +93,58 @@ export class Wallet extends AdapterL2(AdapterL1(ethers.Wallet)) {
         this.providerL1 = providerL1;
     }
 
+    // Method to populate a transaction object with necessary details
     override async populateTransaction(transaction: TransactionRequest): Promise<TransactionLike> {
+        // Default transaction type when custom data is not provided
         if (transaction.type == null && transaction.customData == null) {
-            // use legacy txs by default
             transaction.type = 0;
         }
+
+        // Check for custom data and transaction type
         if (transaction.customData == null && transaction.type != EIP712_TX_TYPE) {
             return (await super.populateTransaction(transaction)) as TransactionLike;
         }
+
+        // Set transaction type and prepare data for EIP712
         transaction.type = EIP712_TX_TYPE;
         const populated = (await super.populateTransaction(transaction)) as TransactionLike;
 
+        // Set default values for transaction fields if not provided
         populated.type = EIP712_TX_TYPE;
         populated.value ??= 0;
         populated.data ??= "0x";
-        populated.customData = this._fillCustomData(transaction.customData ?? {});
-        populated.gasPrice = await this.provider.getGasPrice();
+
+        // Retrieve gas price
+        const gasPrice = await this.provider.getGasPrice();
+        populated.gasPrice = gasPrice;
+
         return populated;
     }
 
+    // Method to sign the transaction
     override async signTransaction(transaction: TransactionRequest): Promise<string> {
+        // Default transaction signing when custom data is not provided
         if (transaction.customData == null && transaction.type != EIP712_TX_TYPE) {
             if (transaction.type == 2 && transaction.maxFeePerGas == null) {
                 transaction.maxFeePerGas = await this.provider.getGasPrice();
             }
             return await super.signTransaction(transaction);
         } else {
+            // Handle transactions with custom data and validate the sender's address
             transaction.from ??= this.address;
-            let from = await ethers.resolveAddress(transaction.from);
+            const from = await ethers.resolveAddress(transaction.from);
+
+            // Address matching check
             if (from.toLowerCase() != this.address.toLowerCase()) {
                 throw new Error("Transaction `from` address mismatch");
             }
+
+            // Prepare and sign EIP712 data
             transaction.customData ??= {};
             transaction.customData.customSignature = await this.eip712.sign(transaction);
             const populated = await this.populateTransaction(transaction);
 
             return serializeEip712(populated);
         }
-    }
-
-    override async sendTransaction(tx: TransactionRequest): Promise<TransactionResponse> {
-        const populatedTx = await this.populateTransaction(tx);
-        return await this.provider.broadcastTransaction(await this.signTransaction(populatedTx));
     }
 }
